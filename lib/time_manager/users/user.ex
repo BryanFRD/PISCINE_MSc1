c2a4a08b582f7d312567124925db0faa0eb4e5ee
Natchii59 <caron.nathan@hotmail.com>
2024-10-07 16:33:25 +0200

    chore: init application with users crud

---
+++ lib/time_manager/users/user.ex
@@ -0,0 +1,20 @@
+defmodule TimeManager.Users.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :username, :string
+    field :email, :string
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:username, :email])
+    |> validate_required([:username, :email])
+    |> unique_constraint(:email)
+    |> validate_format(:email, ~r/@/)
+  end
+end
