a3050b8695295ff05c2300b0c3e3d7539e668eeb
Natchii59 <caron.nathan@hotmail.com>
2024-10-08 16:24:18 +0200

    feat: add database seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,47 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias TimeManager.Repo
+alias TimeManager.Users.User
+alias TimeManager.Clocks.Clock
+alias TimeManager.Workingtimes.Workingtime
+
+# Inserting Users
+user1 =
+  Repo.insert!(%User{
+    username: "John Doe",
+    email: "john.doe@example.com"
+  })
+
+user2 =
+  Repo.insert!(%User{
+    username: "Jane Smith",
+    email: "jane.smith@example.com"
+  })
+
+# Inserting Clocks
+Repo.insert!(%Clock{
+  user_id: user1.id,
+  status: true,
+  time: ~N[2024-10-08 08:00:00]
+})
+
+Repo.insert!(%Clock{
+  user_id: user2.id,
+  status: false,
+  time: ~N[2024-10-08 17:00:00]
+})
+
+# Inserting Working Times
+Repo.insert!(%Workingtime{
+  user_id: user1.id,
+  start: ~N[2024-10-08 08:00:00],
+  end: ~N[2024-10-08 17:00:00]
+})
+
+Repo.insert!(%Workingtime{
+  user_id: user2.id,
+  start: ~N[2024-10-08 08:00:00],
+  end: ~N[2024-10-08 18:00:00]
+})
