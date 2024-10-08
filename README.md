6b3f3729c2fa19020a1be46d186b48856257727f
Natchii59 <caron.nathan@hotmail.com>
2024-10-08 16:01:11 +0200

    docs: add workingtimes swagger

--- README.md
+++ README.md
@@ -1,27 +1,22 @@
-
 # TIME MANAGER
 
- Gotham City Hall wants to deploy the application on several platforms (mainly computers and different types of terminals). It is also considering a mobile version.
- The goal of this project is to create a platform-independent API for exchanging and storing data.
-
+Gotham City Hall wants to deploy the application on several platforms (mainly computers and different types of terminals). It is also considering a mobile version.
+The goal of this project is to create a platform-independent API for exchanging and storing data.
 
 ## Tech Stack
 
 **Server:** Elixir
 
-**Frame work:** Phoenix
-
+**Framework:** Phoenix
 
 ## Installation
 
 ### You need :
-- [``Erlang VM``](https://www.erlang.org/downloads.html)
-- [`Elixir`](https://elixir-lang.org/install.html) 
-- [``Phoenix FrameWork``](https://hexdocs.pm/phoenix/up_and_running.html)
-- [``Docker``](https://www.docker.com/products/docker-desktop/)
 
-    
-
+- [`Erlang VM (Windows)`](https://www.erlang.org/downloads.html)
+- [`Elixir`](https://elixir-lang.org/install.html)
+- [`Phoenix Framework`](https://hexdocs.pm/phoenix/up_and_running.html)
+- [`Docker`](https://www.docker.com/products/docker-desktop/)
 
 Clone the project :
 
@@ -34,6 +29,7 @@ Go to the project directory :
 ```bash
   cd T-POO-700-LIL_2
 ```
+
 Run docker Compose to get the workspace :
 
 ```bash
@@ -41,6 +37,7 @@ Run docker Compose to get the workspace :
 ```
 
 To add the dependencies run :
+
 ```bash
   mix deps.get
 ```
@@ -48,7 +45,7 @@ To add the dependencies run :
 To create the database run :
 
 ```bash
-  mix ecto.create  
+  mix ecto.create
 ```
 
 To migrate the database run :
@@ -57,19 +54,19 @@ To migrate the database run :
   mix ecto.migrate
 ```
 
-
 To start the Phoenix server:
+
 ```bash
-  mix phx.server 
+  mix phx.server
 ```
+
 Now you can visit [`localhost:4000`](http://localhost:4000) from your browser.
 
 ## API Reference
 
-#### SWAGGER
+### Swagger
 
 To check the API Reference you can visit [`the swagger`](http://localhost:4000/api/swagger) from your browser.
-
 
 ## Running Tests
 
@@ -79,14 +76,9 @@ To run tests, run the following command
   mix test
 ```
 
-
-
-
-
-
 ## Authors
 
 - [🦍@Tiagovhs🐉](https://github.com/Tiagovhs)
-- [@Natchii59](https://github.com/Natchii59)
+- [@Natchii59🤓](https://github.com/Natchii59)
 - [@BryanFRD](https://github.com/BryanFRD)
 - [@Sarah](https://github.com/dumbfacemonster)
--- lib/time_manager_web/controllers/workingtime_controller.ex
+++ lib/time_manager_web/controllers/workingtime_controller.ex
@@ -1,14 +1,83 @@
 defmodule TimeManagerWeb.WorkingtimeController do
   use TimeManagerWeb, :controller
+  use PhoenixSwagger
 
   alias TimeManager.Workingtimes
   alias TimeManager.Workingtimes.Workingtime
 
   action_fallback TimeManagerWeb.FallbackController
 
+  def swagger_definitions do
+    %{
+      Workingtime:
+        swagger_schema do
+          title("Workingtime")
+          description("A user's working time")
+
+          properties do
+            id(:number, "Unique identifier for the user", required: true)
+            start(:datetime, "The start time of the working time", required: true)
+            end_time(:datetime, "The end time of the working time (field: 'end')", required: true)
+            user(:User, "The user creating this working time", required: true)
+          end
+
+          example(%{
+            id: 1,
+            start: "2024-10-08 08:00:00",
+            end: "2024-10-08 17:00:00",
+            user: %{
+              id: 1,
+              username: "john_doe",
+              email: "john.doe@email.com"
+            }
+          })
+        end,
+      Workingtimes:
+        swagger_schema do
+          title("Workingtimes")
+          description("A collection of Workingtimes")
+          type(:array)
+          items(Schema.ref(:Workingtime))
+        end,
+      WorkingtimeAttributes:
+        swagger_schema do
+          title("WorkingtimeAttributes")
+          description("Attributes for creating or updating a workingtime")
+
+          properties do
+            start(:datetime, "The start time of the working time", required: true)
+            end_time(:datetime, "The end time of the working time", required: true)
+          end
+
+          example(%{
+            workingtime: %{
+              start: "2024-10-08 08:00:00",
+              end: "2024-10-08 17:00:00"
+            }
+          })
+        end
+    }
+  end
+
   def index(conn, %{"user_id" => user_id} = params) do
     workingtimes = Workingtimes.list_user_workingtimes(user_id, params)
     render(conn, :index, workingtimes: workingtimes)
+  end
+
+  swagger_path :index do
+    get("/workingtimes/{user_id}")
+    description("List all user workingtimes")
+
+    parameters do
+      user_id(:path, :string, "Unique identifier for the user", required: true)
+      start(:query, :datetime, "The start time of the working time", required: false)
+
+      end_time(:query, :datetime, "The end time of the working time (field: 'end')",
+        required: false
+      )
+    end
+
+    response(200, "List of user workingtimes", Schema.ref(:Workingtimes))
   end
 
   def create(conn, %{"user_id" => user_id, "workingtime" => workingtime_params}) do
@@ -23,9 +92,39 @@ defmodule TimeManagerWeb.WorkingtimeController do
     end
   end
 
+  swagger_path :create do
+    post("/workingtimes/{user_id}")
+    description("Create a new workingtime for a user")
+
+    parameters do
+      user_id(:path, :string, "Unique identifier for the user", required: true)
+
+      workingtime(:body, Schema.ref(:WorkingtimeAttributes), "Workingtime attributes",
+        required: true
+      )
+    end
+
+    response(201, "Successfuly created workingtime", Schema.ref(:Workingtime))
+    response(400, "Bad request")
+    response(422, "Unprocessable entity")
+  end
+
   def show(conn, %{"user_id" => user_id, "id" => id}) do
     workingtime = Workingtimes.get_user_workingtime!(user_id, id)
     render(conn, :show, workingtime: workingtime)
+  end
+
+  swagger_path :show do
+    get("/workingtimes/{user_id}/{id}")
+    description("Show a single workingtime")
+
+    parameters do
+      user_id(:path, :string, "Unique identifier for the user", required: true)
+      id(:path, :string, "Unique identifier for the workingtime", required: true)
+    end
+
+    response(200, "Workingtime", Schema.ref(:Workingtime))
+    response(404, "Workingtime not found")
   end
 
   def update(conn, %{"id" => id, "workingtime" => workingtime_params}) do
@@ -37,11 +136,41 @@ defmodule TimeManagerWeb.WorkingtimeController do
     end
   end
 
+  swagger_path :update do
+    put("/workingtimes/{id}")
+    description("Update workingtime")
+
+    parameters do
+      id(:path, :string, "Unique identifier for the workingtime", required: true)
+
+      workingtime(:body, Schema.ref(:WorkingtimeAttributes), "Workingtime attributes",
+        required: true
+      )
+    end
+
+    response(200, "Updated workingtime", Schema.ref(:Workingtime))
+    response(400, "Bad request")
+    response(404, "Workingtime not found")
+    response(422, "Unprocessable entity")
+  end
+
   def delete(conn, %{"id" => id}) do
     workingtime = Workingtimes.get_workingtime!(id)
 
     with {:ok, %Workingtime{}} <- Workingtimes.delete_workingtime(workingtime) do
       send_resp(conn, :no_content, "")
     end
+  end
+
+  swagger_path :delete do
+    PhoenixSwagger.Path.delete("/workingtimes/{id}")
+    description("Delete workingtime")
+
+    parameters do
+      id(:path, :string, "Unique identifier for the workingtime", required: true)
+    end
+
+    response(200, "Successfully deleted")
+    response(404, "Workingtime not found")
   end
 end
